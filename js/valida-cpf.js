07b7dca6f3b5d3f325e2645ddccd94f54776893d
Vinicius Pereira Macedo <viniciusp.macedo@gmail.com>
2023-05-27 14:51:33 -0300

    validação cpf e idade

---
+++ js/valida-cpf.js
@@ -0,0 +1,67 @@
+export default function ehUmCPF(campo) {
+    const cpf = campo.value.replace(/\.|-/g, "");
+    
+    if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)){
+        console.log("CPF inválido!")
+    } else {
+        console.log("Existe.")
+    }
+    
+}
+
+function validaNumerosRepetidos(cpf) {
+    const numerosRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ];
+
+    return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
+    }
+
+    soma = soma % 11;
+
+    if (soma < 2) {
+        soma = 0;
+    } else {
+        soma = 11 - soma
+    }
+    
+    return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
+    }
+
+    soma = soma % 11;
+
+    if (soma < 2) {
+        soma = 0;
+    } else {
+        soma = 11 - soma;
+    }
+    
+    return soma != cpf[10];
+}
